68e971d2c7111288cae1d10ee1fe66c2de17b125
GeorgeDanielConstantin <danielgconstantin@gmail.com>
2023-01-26 16:51:45 +0100

    add extra

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const buttonCalcoloEl = document.getElementById("calcolo_biglietto");
 const inputEtàEl = document.getElementById("Età");
 const inputKmEl = document.getElementById("Km");
-
+const nomeCognome = document.getElementById("NomeCognome")
+const carrozza = Math.floor(Math.random() * 9) + 1;
+const codiceCp = Math.floor(Math.random() * 89999) + 10000;
 
 
 buttonCalcoloEl.addEventListener("click", function () {
@@ -16,6 +18,11 @@ buttonCalcoloEl.addEventListener("click", function () {
     const prezzoEuroKm = 0.21;
     let price = km * prezzoEuroKm;
 
+    document.getElementById('carrozza').innerHTML = carrozza;
+    document.getElementById('codice_cp').innerHTML = codiceCp;
+    document.getElementById('nome_cognome').innerHTML = nomeCognome;
+
+
     // MINORENNE
     if (!isNaN(età) && !isNaN(km)) {
         if (età < 18) {
@@ -23,7 +30,8 @@ buttonCalcoloEl.addEventListener("click", function () {
             const scontoMinorenneEuro = price * 0.2;
             const pricef = price - scontoMinorenneEuro;
 
-            console.log(`Prezzo under 18 ${pricef.toFixed(2)}`);
+            document.getElementById('tipo_biglietto').innerHTML = `Biglietto under 18`;
+            document.getElementById('output_prezzo').innerHTML = `${pricef.toFixed(2)}`;
         }
         // ANZIANO
 
@@ -32,14 +40,17 @@ buttonCalcoloEl.addEventListener("click", function () {
             const scontoAnzianoEuro = price * 0.4;
             const pricef = price - scontoAnzianoEuro;
 
-            console.log(`Prezzo over 65 ${pricef.toFixed(2)}`);
+            document.getElementById('tipo_biglietto').innerHTML = `Biglietto over 65`;
+            document.getElementById('output_prezzo').innerHTML = `${pricef.toFixed(2)}`;
         }
 
         if (età >= 18 && età < 65) {
             const pricef = price
-            console.log(`Prezzo ${pricef.toFixed(2)}`);
+
+            document.getElementById('tipo_biglietto').innerHTML = `Biglietto ordinario`;
+            document.getElementById('output_prezzo').innerHTML = `${pricef.toFixed(2)}`;
         }
     } else {
-        console.log('Valori invalidi');
+        alert('Valori invalidi');
     }
-}) 
+})
